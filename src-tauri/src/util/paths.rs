cb1b2514aa8298e1bbaab3f8405402675bbf2c87
Mario <mariolopjr@users.noreply.github.com>
2026-03-23 22:30:51 -0400

    rustfmt

--- src-tauri/src/util/paths.rs
+++ src-tauri/src/util/paths.rs
@@ -82,7 +82,10 @@ pub fn get_plugins_path() -> PathBuf {
 pub fn get_saves_dir() -> PathBuf {
   let path = if config_is_local() {
     let exe = std::env::current_exe().unwrap_or_default();
-    exe.parent().unwrap_or(std::path::Path::new(".")).join("saves")
+    exe
+      .parent()
+      .unwrap_or(std::path::Path::new("."))
+      .join("saves")
   } else {
     let appdata = dirs::data_dir().unwrap_or_default();
     appdata.join("roguetop").join("saves")
